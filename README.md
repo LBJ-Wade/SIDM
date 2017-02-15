eadb8f1b89da5011a7cf1e78983c044fb523aaa6
Ryan Keeley <ryankeeley@ncanac.local>
2017-02-15 14:45:33 -0800

    added command line arguments and put no baryon case in SIDM_pde.py

--- README.md
+++ README.md
@@ -2,6 +2,29 @@
 
 SIDM_pde.py calculates the log of the density of DM in the presence of some baryon potential
 
-SIDM_pde_nobaryons.py same as above but no baryons, which gets the isothermal core solution
+it includes optional use of command line arguments:
+--output: type = str, default = None,
+    help = 'names (prefix) of output files to write')
+--r_len: type = int, default=50,
+    help = 'size of array along r direction')
+--z_len: type = int, default=50,
+    help = 'size of array along r direction')
+--scale: type = float, default=-4,
+    help = 'exponential term to describe innermost point r_inner = r1 * 10^scale')
+--r1: type = float, default=5.,
+    help = 'the boundary of the array in kpc')
+--rho0: type = float, default=5.e6,
+    help = 'scale density of the solution in M_sun/kpc')
+--num_iter: type = int, default=1000000,
+    help = 'number of iterations in the relaxation')
+--no_baryons: action='store_true',
+    help = 'type --no_baryons if you want an isothermal core')
 
-all the plots with _iso suffix comes from the nobaryons solution
+example command line:
+python SIDM_pde.py --r_len 100 --z_len 100 --scale -5. --r1 10. --rho0 1.e7 --num_iter 4000000 --no_baryons
+
+also writes a .txt file with the parameters used
+
+no longer includes no baryon case as a seperate file since that functionality is now in an if statement    
+    
+as of 2/15/17 all the plots with _iso suffix comes from the nobaryons solution
--- SIDM_pde.py
+++ SIDM_pde.py
@@ -2,6 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
+import argparse
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--output',type = str, default = None,
+    help = 'names (prefix) of output files to write')
+parser.add_argument('--r_len', type = int, default=50,
+    help = 'size of array along r direction')
+parser.add_argument('--z_len', type = int, default=50,
+    help = 'size of array along r direction')
+parser.add_argument('--scale', type = float, default=-4,
+    help = 'exponential term to describe innermost point r_inner = r1 * 10^scale')
+parser.add_argument('--r1', type = float, default=5.,
+    help = 'the boundary of the array in kpc')
+parser.add_argument('--rho0', type = float, default=5.e6,
+    help = 'scale density of the solution in M_sun/kpc')
+parser.add_argument('--num_iter', type = int, default=1000000,
+    help = 'number of iterations in the relaxation')
+parser.add_argument('--no_baryons', action='store_true',
+    help = 'type --no_baryons if you want an isothermal core')
+args = parser.parse_args()
+
+
 def interp(grid):
     grid_shape = grid.shape
     new_grid_shape = grid_shape*2
@@ -52,26 +75,58 @@ def relax(u, r, z, r1, rho0, iterations):
         u[:,-1] = NFW_template(r[:,-1],z[:,-1]) #used to enforce 'NFW for large R' boundary condition 
     return u, residual
 
+def relax_no_baryons(u, r, z, r1, rho0, iterations):
+    #u is the solution to the diffeq, given in terms of log(rho/rho0)
+    g= 2.71*(rho0/5.e6)*(r1/10.)**2 # 4pi * GN * rho0 *r1**2/sigma**2 for sigma 100 km/s, it is dimensionless  
+    delta_r = np.gradient(r/r1)[0]  # for a 2-d array, gradient returns gradient as a list 'vector' defined at each point, 0 axis for r and 1 axis for z   
+    delta_z = np.gradient(z/r1)[1]
+    #time_step = 1/10000.
+    for i in range(iterations):
+        u[0,:] = u[1,:] 
+        u[:,0] = u[:,1] 
+        #enforces the derivative goes to 0 at r,z = 0, which is required by symmetry / smoothness
+        residual = r1/r*np.gradient(u)[0]/delta_r + np.gradient(u,2)[0]/delta_r**2 + np.gradient(u,2)[1]/delta_z**2 + g*(np.exp(u))
+        time_step = 1e-3/residual.max()
+        u += time_step*residual
+        u[-1,:] = NFW_template(r[-1,:],z[-1,:])
+        u[:,-1] = NFW_template(r[:,-1],z[:,-1]) #used to enforce 'NFW for large R' boundary condition 
+    return u, residual
 
-r_len = 26
-z_len = 25
-bound = 1
-scale = -4. #exponential term to describe innermost point r_inner = r1 * 10^scale
-r1 = 5. # in kpc 
-rho0 = 5.e6 # scale density of the solution in M_sun/kpc^3
-num_iter = 1000000
+
+
+#r_len = 51
+#z_len = 50
+#scale = -4. #exponential term to describe innermost point r_inner = r1 * 10^scale
+#r1 = 5. # in kpc
+#rho0 = 5.e6 # scale density of the solution in M_sun/kpc^3
+#num_iter = 4000000
+r_len = args.r_len
+z_len = args.z_len
+scale = args.scale
+r1 = args.r1
+rho0 = args.rho0
+num_iter = args.num_iter
+output = args.output
+
+f1=open(output+'_params.txt', 'w')
+f1.write('len in r direction is '+str(r_len))
+f1.write('len in z direction is '+str(z_len))
+f1.write('magnitude of innermost point is '+str(scale))
+f1.write('outer boundary is '+str(r1)+' kpc')
+f1.write('number of iterations is '+str(num_iter))
+f1.close()
 
 
 #arrays (r,z,u) have the shape (r_len,z_len)  
   
-r = bound*r1*np.logspace(scale,0.,r_len,endpoint=True) #the polar radius; varies from r1*( e^-scale to 1), the [::-1] is there to reverse the array to make the index start at the inner part of the halo
+r = r1*np.logspace(scale,0.,r_len,endpoint=True) #the polar radius; varies from r1*( e^-scale to 1), the [::-1] is there to reverse the array to make the index start at the inner part of the halo
 r = np.tile(r[:,np.newaxis],(1,z_len))
 
-z = bound*r1*np.logspace(scale,0.,z_len,endpoint=True) #the height; varies from r1*( e^-scale to 1), the [::-1] is there to reverse the array to make the index start at the inner part of the halo
+z = r1*np.logspace(scale,0.,z_len,endpoint=True) #the height; varies from r1*( e^-scale to 1), the [::-1] is there to reverse the array to make the index start at the inner part of the halo
 z = np.tile(z[np.newaxis,:],(r_len,1))
 
 
-u = NFW_template(r, z) #u is the solution to the diffeq, given in terms of log(rho/rho0)       
+u = NFW_template(r, z) #u is the solution to the diffeq, given in terms of log(rho/rho0); this line sets up the initial condition for the pde
 
 
 #### PLOTS BEFORE RELAXATION ####
@@ -80,21 +135,21 @@ ax = fig.add_subplot(111, projection='3d')
 ax.plot_wireframe(np.log(r), np.log(z), u, rstride=2, cstride=2)
 plt.xlabel(r'$\log(r)$')
 plt.ylabel(r'$\log(z)$')
-plt.savefig('wireplot_before.png')
+plt.savefig(output + 'wireplot_before.png')
 plt.clf()
 
 CS = plt.contour(np.log(r),np.log(z),u)
 plt.clabel(CS, inline=1,fontsize=10)
 plt.xlabel(r'$\log(r)$')
 plt.ylabel(r'$\log(z)$')
-plt.savefig('log_density_before.png')
+plt.savefig(output + 'log_density_before.png')
 plt.clf()
 
 CS = plt.contour(r,z,u)
 plt.clabel(CS, inline=1,fontsize=10)
 plt.xlabel('Radius (kpc)')
 plt.ylabel('Height (z) (kpc)')
-plt.savefig('density_before.png')
+plt.savefig(output + 'density_before.png')
 plt.clf()
 
 plt.contourf(r,z,u)
@@ -102,15 +157,17 @@ plt.xscale('log')
 plt.yscale('log')
 plt.xlabel(r'$\log(r)$')
 plt.ylabel(r'$\log(z)$')
-plt.savefig('log_density_filled_before.png')
+plt.savefig(output + 'log_density_filled_before.png')
 plt.clf()
 
 
 
 
 #### RELAXATION ####
-u, residual = relax(u, r, z, r1, rho0, num_iter)
-
+if args.no_baryons:
+    u, residual = relax_no_baryons(u, r, z, r1, rho0, num_iter)
+else:
+    u, residual = relax(u, r, z, r1, rho0, num_iter)
 
 
 #### PLOTS AFTER RELAXATION ####
@@ -118,14 +175,14 @@ CS = plt.contour(np.log(r),np.log(z),u)
 plt.clabel(CS, inline=1, fontsize=10)
 plt.xlabel(r'$\log(r)$')
 plt.ylabel(r'$\log(z)$')
-plt.savefig('log_density_after.png')
+plt.savefig(output + 'log_density_after.png')
 plt.clf()
 
 CS = plt.contour(r,z,u)
 plt.clabel(CS, inline=1, fontsize=10)
 plt.xlabel('Radius (kpc)')
 plt.ylabel('Height (z) (kpc)')
-plt.savefig('density_after.png')
+plt.savefig(output + 'density_after.png')
 plt.clf()
 
 fig = plt.figure()
@@ -133,7 +190,7 @@ ax = fig.add_subplot(111, projection='3d')
 ax.plot_wireframe(np.log(r), np.log(z), u, rstride=2, cstride=2)
 plt.xlabel(r'$\log(r)$')
 plt.ylabel(r'$\log(z)$')
-plt.savefig('wireplot_after.png')
+plt.savefig(output + 'output + wireplot_after.png')
 plt.clf()
 
 plt.contourf(r,z,u)
@@ -141,7 +198,7 @@ plt.xscale('log')
 plt.yscale('log')
 plt.xlabel(r'$\log(r)$')
 plt.ylabel(r'$\log(z)$')
-plt.savefig('log_density_filled_after.png')
+plt.savefig(output + 'log_density_filled_after.png')
 plt.clf()
 
 
@@ -150,7 +207,7 @@ CS = plt.contour(np.log(r),np.log(z),residual)
 plt.clabel(CS, inline=1, fontsize=10)
 plt.xlabel(r'$\log(r)$')
 plt.ylabel(r'$\log(z)$')
-plt.savefig('final_residual.png')
+plt.savefig(output + 'final_residual.png')
 plt.clf()
 
 fig = plt.figure()
@@ -158,14 +215,14 @@ ax = fig.add_subplot(111, projection='3d')
 ax.plot_wireframe(np.log(r), np.log(z), residual, rstride=2, cstride=2)
 plt.xlabel(r'$\log(r)$')
 plt.ylabel(r'$\log(z)$')
-plt.savefig('wireplot_residual.png')
+plt.savefig(output + 'wireplot_residual.png')
 plt.clf()
 
 CS = plt.contour(np.log(r),np.log(z),residual/u)
 plt.clabel(CS, inline=1, fontsize=10)
 plt.xlabel(r'$\log(r)$')
 plt.ylabel(r'$\log(z)$')
-plt.savefig('final_residual_contrast.png')
+plt.savefig(output + 'final_residual_contrast.png')
 plt.clf()
 
 fig = plt.figure()
@@ -173,6 +230,6 @@ ax = fig.add_subplot(111, projection='3d')
 ax.plot_wireframe(np.log(r), np.log(z), residual/u, rstride=2, cstride=2)
 plt.xlabel(r'$\log(r)$')
 plt.ylabel(r'$\log(z)$')
-plt.savefig('wireplot_residual_contrast.png')
+plt.savefig(output + 'wireplot_residual_contrast.png')
 plt.clf()
                 
